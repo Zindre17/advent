0ace269acc599a118e6ec4285e9baecd1872c6f0
Sindre Aarstrand <saarstrand@akvagroup.com>
2025-12-08 14:35:03 +0100

    Add solution for day 5 part 2

--- 2025/ada/day_5.adb
+++ 2025/ada/day_5.adb
@@ -1,5 +1,7 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+with Ada.Containers; use Ada.Containers;
+with Ada.Containers.Vectors;
 
 procedure Day_5 is
   type Long is mod 2 ** 64;
@@ -10,6 +12,9 @@ procedure Day_5 is
   end record;
   type Range_Array is array (Integer range <>) of Range_Record;
 
+  package Range_Vectors is new Ada.Containers.Vectors(Index_Type => Positive, Element_Type => Range_Record);
+  use Range_Vectors;
+
   Input_File_Name : constant String := "day5.txt";
   Input_File : File_Type;
 
@@ -18,6 +23,39 @@ procedure Day_5 is
 
   Fresh : Boolean;
   Fresh_Ingredient_Count : Integer := 0;
+
+  Processed_Ranges : Vector;
+
+  function Min(A : Long; B : Long) return Long is
+  begin
+    if A < B then
+      return A;
+    else
+      return B;
+    end if;
+  end;
+
+  function Max(A : Long; B : Long) return Long is
+  begin
+    if A > B then
+      return A;
+    else
+      return B;
+    end if;
+  end;
+
+  function To_String(R : Range_Record) return String is
+  begin
+    return R.First'Image & " -" & R.Last'Image;
+  end;
+
+  function Intersects(A : Range_Record; B : Range_Record) return Boolean is
+  begin
+    return (A.First <= B.Last and A.Last >= B.First)
+        or (B.First <= A.Last and B.Last >= A.First)
+        or (A.First >= B.First and A.Last <= B.Last)
+        or (B.First >= A.First and B.Last <= A.Last);
+  end;
 begin
   Open(Input_File, In_File, Input_File_Name);
   loop
@@ -77,7 +115,56 @@ begin
       end if;
     end loop;
 
-    Put_Line("Fresh ingredients:" & Fresh_Ingredient_Count'Image);
+    for Current_Range of Ranges loop
+      declare
+        Did_Update : Boolean := False;
+      begin
+        for R of Processed_Ranges loop
+          if Intersects(Current_Range, R) then
+            R.First := Min(Current_Range.First, R.First);
+            R.Last := Max(Current_Range.Last, R.Last);
+            Did_Update := True;
+            exit;
+          end if;
+        end loop;
+
+        if not Did_Update then
+          Processed_Ranges.Append(Current_Range);
+        end if;
+      end;
+    end loop;
+
+    for I in 1 .. Processed_Ranges.Length loop
+      for J in 1 .. Processed_Ranges.Length loop
+        declare
+          A : Range_Record := Processed_Ranges(Positive(I));
+          B : Range_Record := Processed_Ranges(Positive(J));
+        begin
+          if I /= J and Intersects(A, B) then
+            Processed_Ranges(Positive(I)).First := Min(A.First, B.First);
+            Processed_Ranges(Positive(I)).Last := Max(A.Last, B.Last);
+            Processed_Ranges(Positive(J)).First := 0;
+            Processed_Ranges(Positive(J)).Last := 0;
+          end if;
+        end;
+      end loop;
+    end loop;
+
+    for I in reverse 1 ..Processed_Ranges.Length loop
+      if Processed_Ranges(Positive(I)).First = 0 and Processed_Ranges(Positive(I)).Last = 0 then
+        Processed_Ranges.Delete(Positive(I));
+      end if;
+    end loop;
+
+    declare
+      Id_Count : Long := 0;
+    begin
+      for R of Processed_Ranges loop
+        Id_Count := Id_Count + (R.Last - R.First) + 1;
+      end loop;
+      Put_Line("Fresh ingredients:" & Fresh_Ingredient_Count'Image);
+      Put_Line("Fresh ingredient IDs:" & Id_Count'Image);
+    end;
   end;
 
 
